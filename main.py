58a89d4a0fc0d7e9145e8ce0d0eea39a3f6cfaa1
Scott Neibarger <scott.neibarger@gmail.com>
2023-03-18 22:14:40 -0400

    Updates.

--- main.py
+++ main.py
@@ -5,40 +5,38 @@ from pyMud.globals import area_api, room_api
 from pyMud.rest import *
 
 
+def load_rooms(area_id, rooms):
+    new_rooms = []
+    for room in rooms:
+        room_payload = generate_room_payload(room, generate_mongo_id())
+        room_payload['areaId'] = area_id
+        response = post(room_payload, room_api + "room")
+        content = json.loads(response.content)
+        new_rooms.append(content)
+    return new_rooms
+
+
+def load_area(areas, area_file):
+    sections = parse_area_file(area_file)
+    area_info = extract_area_fields(sections['AREA'][2])
+    areas[area_info['name']] = area_info
+    area = new_area_payload(area_info)
+    response = post(area, area_api + "areas")
+    content = json.loads(response.content)
+    return sections, str(content['id'])
+
+
 def main():
     areas = {}
     area_dir = "C:\\Users\\scott\\CLionProjects\\rom24-quickmud\\area"
     for filename in os.listdir(area_dir):
         if filename.endswith('.are'):
-            area_file = os.path.join(area_dir, filename)
-            sections = parse_area_file(area_file)
-            area_id = None
-            # for section in sections:
-            #     if "AREA" == section:
-            area_info = extract_area_fields(sections['AREA'][2])
-            areas[area_info['name']] = area_info
-            area = new_area_payload(area_info)
-            response = post(area, area_api+"areas")
-            content = json.loads(response.content)
-            area_id = str(content['id'])
-                # if "MOBILES" == section:
-                #     # print("MOBILES: "+str(sections[section]))
-                #     mobiles = extract_mobiles(sections[section])
-                #     print("MOBILES: " + str(mobiles))
-                # if "OBJECTS" == section:
-                #     objects = extract_objects(sections[section])
-                #     print("OBJECTS: " + str(objects))
-                # if "ROOMS" == section:
-            rooms = extract_rooms(sections['ROOMS'])
-            print("ROOMS: " + str(rooms))
-            for room in rooms:
-                print("ROOM: "+str(room))
-                room_payload = generate_room_payload(room, generate_mongo_id())
-                room_payload['areaId'] = area_id
-                print("ROOM_PAYLOAD: "+str(room_payload))
-                response = post(room_payload, room_api+"room")
-                content = json.loads(response.content)
-                print("NEW_ROOM: "+str(content))
+            sections, area_id = load_area(areas, os.path.join(area_dir, filename))
+            load_rooms(area_id, extract_rooms(sections['ROOMS']))
+            mobiles = extract_mobiles(sections['MOBILES'])
+            print("MOBILES: " + str(mobiles))
+            objects = extract_objects(sections['OBJECTS'])
+            print("OBJECTS: " + str(objects))
             break
 
 
